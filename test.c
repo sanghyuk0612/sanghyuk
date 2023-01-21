1567669f911d405cc2d05878f2ec836cc6e3af6c
sanghyuk0612 <hwhwhw944@gmail.com>
2023-01-21 22:26:30 +0900

    Create test.c

---
+++ test.c
@@ -0,0 +1,30 @@
+﻿#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+// prices_len은 배열 prices의 길이입니다.
+int* solution(int prices[], size_t prices_len) {
+    // return 값은 malloc 등 동적 할당을 사용해주세요. 할당 길이는 상황에 맞게 변경해주세요.
+    int* a = (int*)malloc(sizeof(prices));
+    int answer[prices_len];
+    answer[0] = 1;
+    int cnt = 1;
+    for (int i = 1; i < prices_len; i++) {
+        if (a[i] >= a[i - 1]) {
+
+            for (int j = 0; j < i; j++) {
+                cnt++;
+                answer[j] = answer[j] + cnt;
+                
+            }
+        }
+        else
+            cnt = 0;
+        
+    }
+
+    return answer;
+}
+int main(void) {
+
+}
